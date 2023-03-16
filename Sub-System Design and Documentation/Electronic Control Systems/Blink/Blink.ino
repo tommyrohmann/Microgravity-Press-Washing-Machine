c144c59efa3d729379389fec90d0066d76da23c4
Tommy Rohmann <63476994+tommyrohmann@users.noreply.github.com>
2023-03-16 10:27:22 -0400

    Working Ball Valve Actuators

--- Sub-System Design and Documentation/Electronic Control Systems/Blink/Blink.ino
+++ Sub-System Design and Documentation/Electronic Control Systems/Blink/Blink.ino
@@ -1,37 +1,155 @@
-/*
-  Blink
+#include <Servo.h>
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+#define VRX_PIN      A0 // Arduino pin connected to VRX pin
+#define VRY_PIN      A1 // Arduino pin connected to VRY pin
+#define SERVO_X_PIN  2  // Arduino pin connected to Servo motor 1
+#define SERVO_Y_PIN  3  // Arduino pin connected to Servo motor 2
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+Servo xServo;  // create servo object to control a servo 1
+Servo yServo;  // create servo object to control a servo 2
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600) ;
+  xServo.attach(SERVO_X_PIN);
+  yServo.attach(SERVO_Y_PIN);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  // read analog X and Y analog values
+  int xValue = analogRead(VRX_PIN);
+  int yValue = analogRead(VRY_PIN);
+
+  int xAngle = map(xValue, 0, 1023, 0, 180); // scale it to the servo's angle (0 to 180)
+  int yAngle = map(yValue, 0, 1023, 0, 180); // scale it to the servo's angle (0 to 180)
+
+  xServo.write(xAngle); // rotate servo motor 1
+  yServo.write(yAngle); // rotate servo motor 2
+
+  // print data to Serial Monitor on Arduino IDE
+  Serial.print("Joystick: ");
+  Serial.print(xValue);
+  Serial.print(", ");
+  Serial.print(yValue);
+  Serial.print(" => Servo Motor: ");
+  Serial.print(xAngle);
+  Serial.print("°, ");
+  Serial.print(yAngle);
+  Serial.println("°");
+}
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-joystick-servo-motor
+ */
+
+#include <Servo.h>
+#include <ezButton.h>
+
+#define VRX_PIN      A0 // Arduino pin connected to VRX pin
+#define VRY_PIN      A1 // Arduino pin connected to VRY pin
+#define SW_PIN   2  // Arduino pin connected to SW  pin
+#define SERVO_X_PIN  2  // Arduino pin connected to Servo motor 1
+#define SERVO_Y_PIN  3  // Arduino pin connected to Servo motor 2
+
+#define COMMAND_NO     0x00
+#define COMMAND_LEFT   0x01
+#define COMMAND_RIGHT  0x02
+#define COMMAND_UP     0x04
+#define COMMAND_DOWN   0x08
+
+#define LEFT_THRESHOLD  400
+#define RIGHT_THRESHOLD 800
+#define UP_THRESHOLD    400
+#define DOWN_THRESHOLD  800
+
+#define UPDATE_INTERVAL 100 // 100ms
+
+ezButton button(SW_PIN);
+Servo xServo;  // create servo object to control a servo 1
+Servo yServo;  // create servo object to control a servo 2
+
+int xValue = 0 ; // To store value of the X axis
+int yValue = 0 ; // To store value of the Y axis
+int xAngle = 90; // the center position of servo #1
+int yAngle = 90; // the center position of servo #2
+int command = COMMAND_NO;
+
+unsigned long lastUpdateTime = 0;
+
+void setup() {
+  Serial.begin(9600) ;
+
+  xServo.attach(SERVO_X_PIN);
+  yServo.attach(SERVO_Y_PIN);
+
+  button.setDebounceTime(50); // set debounce time to 50 milliseconds
+}
+
+void loop() {
+  button.loop(); // MUST call the loop() function first
+
+  if (millis() - lastUpdateTime > UPDATE_INTERVAL) {
+    lastUpdateTime = millis() ;
+
+    // read analog X and Y analog values
+    xValue = analogRead(VRX_PIN);
+    yValue = analogRead(VRY_PIN);
+
+    // converts the analog value to commands
+    // reset commands
+    command = COMMAND_NO;
+
+    // check left/right commands
+    if (xValue < LEFT_THRESHOLD)
+      command = command | COMMAND_LEFT;
+    else if (xValue > RIGHT_THRESHOLD)
+      command = command | COMMAND_RIGHT;
+
+    // check up/down commands
+    if (yValue < UP_THRESHOLD)
+      command = command | COMMAND_UP;
+    else if (yValue > DOWN_THRESHOLD)
+      command = command | COMMAND_DOWN;
+
+    // NOTE: AT A TIME, THERE MAY BE NO COMMAND, ONE COMMAND OR TWO COMMANDS
+
+    // print command to serial and process command
+    if (command & COMMAND_LEFT) {
+      Serial.println("COMMAND LEFT");
+      xAngle--;
+    }
+
+    if (command & COMMAND_RIGHT) {
+      Serial.println("COMMAND RIGHT");
+      xAngle++;
+    }
+
+    if (command & COMMAND_UP) {
+      Serial.println("COMMAND UP");
+      yAngle--;
+    }
+
+    if (command & COMMAND_DOWN) {
+      Serial.println("COMMAND DOWN");
+      yAngle++;
+    }
+  }
+
+  if (button.isPressed()) {
+    Serial.println("The button is pressed");
+    xAngle = 90; // the center position of servo #1
+    yAngle = 90; // the center position of servo #2
+  }
+
+  xServo.write(xAngle); // rotate servo motor 1
+  yServo.write(yAngle); // rotate servo motor 2
+
+  // print data to Serial Monitor on Arduino IDE
+  Serial.print("Servo Motor's Angle: ");
+  Serial.print(xAngle);
+  Serial.print("°, ");
+  Serial.print(xAngle);
+  Serial.println("°");
 }
