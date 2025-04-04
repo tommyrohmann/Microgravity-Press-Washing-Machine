8562a845d89cee0f269db06a42a7fc952af500aa
Tommy Rohmann <tommyrohmann@gmail.com>
2025-04-03 22:03:37 -0400

    4/3/25

---
+++ System Design and Documentation/CAD Design Work/Old/Inventor Design Project/Actuated Ball Valves/Servo_Sweep_Actuation_Test_Code/Servo_Sweep_Actuation_Test_Code.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);
+  Serial.begin(9600);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  while (1==1) {
+        myservo.write(45);              // tell servo to go to position in variable 'pos'
+    delay(10);
+    myservo.write(135);
+    delay(10);
+    Serial.print(1);
+  }
+}
+
